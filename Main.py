c3c924e77026aad249dee36a26b52f0268fef740
SRIKHANTH R <139539680+Srikhanth14@users.noreply.github.com>
2023-12-28 22:23:30 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -16,7 +16,7 @@ selected=option_menu(
                     icons=["house-fill","book","bar-chart-fill", "card-list"],
                     menu_icon="menu-up",
                     default_index=0,
-                    orientation="horizontal"
+                    #orientation="horizontal"
                     )
 
 if selected=="Floral Welcome":
