dc0432719b8051f2431dfe090768a40cb55bdd6e
SRIKHANTH R <139539680+Srikhanth14@users.noreply.github.com>
2023-12-28 22:08:25 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,23 +10,22 @@ from streamlit_option_menu import option_menu
 import floral_welcome, floral_story, floral_data_hub, floral_entry_form
 
 st.set_page_config(page_title="Iris Flower Classification", page_icon="🌺", layout="wide")
-selected=option_menu(
-                        menu_title="Iris Flower",
-                        options=["Floral Welcome", "Floral Story", "Floral Data Dive", "Floral Entry Form"],
-                        icons=["house-fill","book","bar-chart-fill", "card-list"],
-                        menu_icon="menu-up",
-                        default_index=0,
-                        orientation="horizontal"
+selected=option_menu(menu_title="Iris Flower",
+                    options=["Floral Welcome", "Floral Story", "Floral Data Dive", "Floral Entry Form"],
+                    icons=["house-fill","book","bar-chart-fill", "card-list"],
+                    menu_icon="menu-up",
+                    default_index=0,
+                    orientation="horizontal
                     )
 
 
     
 
 if selected=="Floral Welcome":
-    floral_welcome.iris_home()
+  floral_welcome.iris_home()
 if selected=="Floral Story":
-    floral_story.iris_about()
+  floral_story.iris_about()
 if selected=="Floral Data Dive":
-    floral_data_hub.dataset()
+  floral_data_hub.dataset()
 if selected=="Floral Entry Form":
-    floral_entry_form.app()
+  floral_entry_form.app()
